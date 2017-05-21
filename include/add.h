8bfe1c42dcc13718145d9cb37f7f3b9f20467a5b
Anastasiaa Senina <miss.s-anas.itmm@yandex.ru>
2017-05-21 10:59:19 +0400

    add

--- include/add.h
+++ include/add.h
@@ -1,6 +1,36 @@
-#ifndef INCLUDE_ADD_H_
+﻿#ifndef INCLUDE_ADD_H_
 #define INCLUDE_ADD_H_
+#include <vector>
+using std:: vector;
+class Union_Find {
+private:
+  int size;
+  int* set;
+  int* employment_set;
 
-int add(int x, int y);
+public:
+  void Union_set(int number_set1, int number_set2);
+  int Find_set(int number_el);
+  explicit  Union_Find(int s) {
+    size = s;
+    set = new int[s];
+    employment_set = new int[s];
+    for (int i = 0; i < size; ++i) {
+      set[i] = i;
+      employment_set[i] = 1;
+    }
+  }
+  // äëÿ òåñòîâ
+  int GetSize() {
+    return size;
+  }
+  int GetSet(int i) {
+    return set[i];
+  }
+  int GetEmp_Set(int i) {
+    return employment_set[i];
+  }
+};
 
+int* func(int n, int k, int* el);
 #endif  // INCLUDE_ADD_H_
--- src/add.cpp
+++ src/add.cpp
@@ -1,3 +1,31 @@
-int add(int x, int y) {
-  return x + y;
+#include "add.h"
+int Union_Find::Find_set(int number_el) {
+  return set[number_el];
 }
+void Union_Find::Union_set(int number_set1, int number_set2) {
+  if (employment_set[number_set1] >= employment_set[number_set2]) {
+    for (int i = 0; i < size; ++i) {
+      if (set[i] == number_set2)
+        set[i] = number_set1;
+    }
+  }
+}
+
+int* func(int n, int k,vector< std::pair<int,int>> el ) {
+  Union_Find a(n);
+  for (int i = 0; i < n -1; ++i) {
+    a.Union_set(i,i+1);
+  }
+  for (int i = 0; i < el.size(); i++) {
+    if (a.Find_set(el[i].first) == a.Find_set(el[i].second)) {
+      for (int j = 0; j < a.GetSize(); ++j) {
+        if (j != a.Find_set(el[i].second) {
+          a.Union_set(j, a.Find_set(el[i].second));
+        }
+      }
+    }
+  }
+
+}
+
+
